96443d7fd8eb780d8faf30e9e1358b141d35daef
tomek86 <zajacm@gmx.at>
2018-03-13 15:41:33 +0100

    add projekt Task1

---
+++ tasks/task1/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo World!");
+        }
+    }
+}
